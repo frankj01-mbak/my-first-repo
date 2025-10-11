d419eb7c80a232acff614bbec4909b0d6cbcf678
frankj01-mbak <frankmbakam00@gmail.com>
2025-10-11 10:47:57 -0400

    Add files via upload

---
+++ odd_and_even.py
@@ -0,0 +1,18 @@
+
+#   This program creates a list of 15 numbers and determines
+#   whether each number is even or odd using a loop.
+#   The program then prints the result in a clear format.
+
+# Create a list of 15 numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+# Loop through each number in the list
+for number in numbers:
+    
+    # Check if the number is even
+    
+    if number % 2 == 0:
+        print(str(number) + " is even")
+    else:
+        print(str(number) + " is odd")
+        
